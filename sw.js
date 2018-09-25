8626d097371ead860e175c738da76362973d91ba
Filip Rakowski <frakowski@divante.pl>
2018-09-25 16:56:46 +0200

    Reinvented the wheel

--- sw.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.networkFirst()
-);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env;
+const pkg = require('./package.json');
 
-module.exports = {
-  entry: './src/index.js',
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [
       {
-        test: /\.js$/, 
-        exclude: /node_modules/,
-        use: {
-        loader: "babel-loader", 
-        options: {
-          presets: ["@babel/preset-env"]  
-         }
-        }
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
       }
     ]
   },
-  devServer: {
-    port: 3333,
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
   }
 };
+
+module.exports = config;
